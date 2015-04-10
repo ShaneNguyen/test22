94adb9951a407062a223d996ced49468cd2871c5
nguyen tan hau <haunguyenvt89@gmail.com>
2015-04-10 15:32:03 +0700

    devil commit

--- app/src/main/java/example/com/test22/Push.java
+++ app/src/main/java/example/com/test22/Push.java
@@ -4,5 +4,6 @@ package example.com.test22;
  * Created by SnsPlusVN_05 on 4/10/2015.
  */
 public class Push {
-    public static String COMPANY = "CompanyID";
+    public static String COMPANY = "";
+
 }
